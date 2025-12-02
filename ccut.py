8b3ee55c6313305bd746ecb2c6e6682c84e5dfec
moueslati <eng.oueslati.mohamed@gmail.com>
2025-12-02 09:18:30 +0100

    Implement step 2 for implementing the support of a delimiter

--- ccut.py
+++ ccut.py
@@ -15,6 +15,8 @@ def main():
                         help="filename to read")
     parser.add_argument('-f2', action="store_true",
                         help="Returns only the second field of the line")
+    parser.add_argument('-d', "--delimiter", type=str, default='\t',
+                        help="Specify a delimeter")
     
     args = parser.parse_args()
 
@@ -25,21 +27,20 @@ def main():
         else:
             content = sys.stdin.buffer.read()
     except FileNotFoundError:
-        print(f'file {args.filename} does not exist')
+        print(f'file {args.filename} does not exist', file=sys.stderr)
         exit(1)
     except PermissionError:
-        print(f'You do not have permission to read this file {args.filename}')
+        print(f'You do not have permission to read this file {args.filename}', file=sys.stderr)
         exit(1)
     except IOError as e:
-        print(f'An IO error occured : {e}')
+        print(f'An IO error occured : {e}', file=sys.stderr)
 
+    delimiter = args.delimiter
     lines = content.rstrip().split('\n')
     if args.f2:
-        output = "\n".join([l.split()[1] for l in lines])
-        print(output)
+        output = "\n".join([l.split(delimiter)[1] for l in lines if len(l.split(delimiter))>1])
+        print(output, file=sys.stdout)
 
-
-    
 
 if __name__ == '__main__':
     main()
