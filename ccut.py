83a8a922a0939460d63edb7446d6a2c811c09f3e
moueslati <eng.oueslati.mohamed@gmail.com>
2025-12-02 07:52:30 +0100

    Parsing arguments and reading file

---
+++ ccut.py
@@ -0,0 +1,41 @@
+import argparse
+import sys
+
+def read_file(filename):
+    """Reads a file"""
+    with open(filename, 'r') as f:
+        return f.read()
+    
+def main():
+    parser = argparse.ArgumentParser(
+        description="ccut command"
+    )
+
+    parser.add_argument('filename', action="store_true",
+                        help="filename to read")
+    parser.add_argument('-f2', action="store_true",
+                        help="Returns only the second field of the line")
+    
+    args = parser.parse_args()
+
+    content = ''
+    try:
+        if args.filename:
+            content = read_file(args.filename)
+        else:
+            content = sys.stdin.buffer.read()
+    except FileNotFoundError:
+        print(f'file {args.filename} does not exist')
+        exit(1)
+    except PermissionError:
+        print(f'You do not have permission to read this file {args.filename}')
+        exit(1)
+    except IOError as e:
+        print(f'An IO error occured : {e}')
+
+    
+
+    
+
+if __name__ == '__main__':
+    main()
