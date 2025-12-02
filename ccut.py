dda81ef2741303de667ed57d8973a2978882288d
moueslati <eng.oueslati.mohamed@gmail.com>
2025-12-02 08:04:34 +0100

    Implementing step 1 of the coding challenge for building cut command

--- ccut.py
+++ ccut.py
@@ -11,7 +11,7 @@ def main():
         description="ccut command"
     )
 
-    parser.add_argument('filename', action="store_true",
+    parser.add_argument('filename', nargs='?', default=None,
                         help="filename to read")
     parser.add_argument('-f2', action="store_true",
                         help="Returns only the second field of the line")
@@ -33,7 +33,11 @@ def main():
     except IOError as e:
         print(f'An IO error occured : {e}')
 
-    
+    lines = content.rstrip().split('\n')
+    if args.f2:
+        output = "\n".join([l.split()[1] for l in lines])
+        print(output)
+
 
     
 
